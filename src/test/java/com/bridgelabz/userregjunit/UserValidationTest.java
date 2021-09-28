fe336ae9b6a5250ac4baa2b6ddaef51cde1d4079
777shetty <shetty.chetu7@gmail.com>
2021-09-28 22:04:04 +0530

    ADDED:Junit to test validate last name

--- src/test/java/com/bridgelabz/userregjunit/UserValidationTest.java
+++ src/test/java/com/bridgelabz/userregjunit/UserValidationTest.java
@@ -2,7 +2,6 @@ package com.bridgelabz.userregjunit;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 public class UserValidationTest {
 	@Test
 	public void givenFirstName_WhenProper_ShouldReturnTrue() {
@@ -17,9 +16,27 @@ public class UserValidationTest {
 		Assert.assertFalse(isNotValid);
 	}
 	@Test
-	public void givneLastName_WhenLesserThanThreeCharcaters_ShouldReturnFalse() {
+	public void givneFirstName_WhenLesserThanThreeCharcaters_ShouldReturnFalse() {
 		UserValidation userValidation = new UserValidation();
 		boolean isNotValid = userValidation.validateName("ch");
+		Assert.assertFalse(isNotValid);
+	}
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		UserValidation userValidation =new UserValidation();
+		boolean isValid = userValidation.validateName("Shetty");
+		Assert.assertTrue(isValid);
+	}
+	@Test
+	public void givenLastName_WhenFirstLetterIsSmall_ShouldReturnFalse() {
+		UserValidation userValidation =new UserValidation();
+		boolean isNotValid = userValidation.validateName("shetty");
+		Assert.assertFalse(isNotValid);
+	}
+	@Test
+	public void givneLastName_WhenLesserThanThreeCharcaters_ShouldReturnFalse() {
+		UserValidation userValidation = new UserValidation();
+		boolean isNotValid = userValidation.validateName("sh");
 		Assert.assertFalse(isNotValid);
 	}
 
