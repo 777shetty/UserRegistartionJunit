9b3d85ae08cdcfe5120c93b870e045aa5338388d
777shetty <shetty.chetu7@gmail.com>
2021-09-28 22:19:02 +0530

    ADDED:Junit to test validate password with minimum 8 character

--- src/test/java/com/bridgelabz/userregjunit/UserValidationTest.java
+++ src/test/java/com/bridgelabz/userregjunit/UserValidationTest.java
@@ -75,5 +75,11 @@ public class UserValidationTest {
 		boolean isValid = userValidation.validateMobileNumber("91 85695623");
 		Assert.assertFalse(isValid);
 	}
+	@Test
+	public void givenPassword_WhenProper_ShouldReturnTrue() {
+		UserValidation userValidation =new UserValidation();
+		boolean isValid = userValidation.validatePassword("Chethan@7777");
+		Assert.assertTrue(isValid);
+	}
 
 }
