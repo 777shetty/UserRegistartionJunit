96bdf41feb5874852b48f51bbe4797169636cc3d
777shetty <shetty.chetu7@gmail.com>
2021-10-02 16:30:50 +0530

    MODIFIED:Test cases to handle null values and thro custom exceptions given null emoty values

--- src/main/java/com/bridgelabz/userregjunit/UserValidation.java
+++ src/main/java/com/bridgelabz/userregjunit/UserValidation.java
@@ -1,10 +1,69 @@
 package com.bridgelabz.userregjunit;
 
 public class UserValidation {
+	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}";
+	private static final String EMAIL_ID_PATTERN = "^[a-z]+([.]?[a-z0-9_+-]+)?@[a-z1-9]+[.][a-z]{2,}([.][a-z]{2,})?$";
+	private static final String PHONE_NUMBER_PATTERN = "\\d{1,3} \\d{10}$";
+	private static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,})[0-9a-zA-Z]*[^0-9a-zA-Z][0-9a-zA-Z]*$";
 
-	public boolean validateName(String string) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean validateName(String name) throws UserValidatorException {
+		try {
+			if(name.length()==0) {
+				throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_EMPTY,
+						"Please enter a valid name");
+			}
+			return name.matches(NAME_PATTERN);
+		}
+    catch (NullPointerException e) {
+			throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_NULL,
+					"Please enter a valid name");
+		}
+    }
+	public boolean validateEMail(String email) throws UserValidatorException {
+    	try {
+    		if(email.length()==0) {
+				throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_EMPTY,
+						"Please enter a valid email");
+			}
+        return email.matches(EMAIL_ID_PATTERN);
+    	} catch (NullPointerException e) {
+			throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_NULL,
+					"Please enter a valid email");
+		}
+    }
+    public boolean validateMobileNumber1(String mobileNumber)
+    {
+        return mobileNumber.matches(PHONE_NUMBER_PATTERN);
+    }
+    public boolean validatePassword1(String password)
+    {
+        return password.matches(PASSWORD_PATTERN);
+    }
+
+	public boolean validateMobileNumber(String mobileNumber) throws UserValidatorException {
+    	try {
+    		if(mobileNumber.length()==0) {
+				throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_EMPTY,
+						"Please enter a valid mobile number");
+			}
+		return mobileNumber.matches(PHONE_NUMBER_PATTERN);
+	} catch (NullPointerException e) {
+		throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_NULL,
+				"Please enter a valid mobile number");
+	}
 	}
 
+	public boolean validatePassword(String password) throws UserValidatorException {
+    	try {
+    		if(password.length()==0) {
+				throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_EMPTY,
+						"Please enter a valid password");
+			}
+		return password.matches(PASSWORD_PATTERN);
+	} catch (NullPointerException e) {
+		throw new UserValidatorException(UserValidatorException.ExceptionType.ENTERED_NULL,
+				"Please enter a password");
+	}
+	}
 }
+
--- src/test/java/com/bridgelabz/userregjunit/UserValidationTest.java
+++ src/test/java/com/bridgelabz/userregjunit/UserValidationTest.java
@@ -1,26 +1,379 @@
 package com.bridgelabz.userregjunit;
 
+
 import org.junit.Assert;
 import org.junit.Test;
-
 public class UserValidationTest {
 	@Test
-	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		UserValidation userValidation =new UserValidation();
-		boolean isValid = userValidation.validateName("Chethan");
-		Assert.assertTrue(isValid);
-	}
-	@Test
-	public void givenFirstName_WhenFirstLetterIsSmall_ShouldReturnFalse() {
-		UserValidation userValidation =new UserValidation();
-		boolean isNotValid = userValidation.validateName("chethan");
-		Assert.assertFalse(isNotValid);
-	}
-	@Test
-	public void givneLastName_WhenLesserThanThreeCharcaters_ShouldReturnFalse() {
-		UserValidation userValidation = new UserValidation();
-		boolean isNotValid = userValidation.validateName("ch");
-		Assert.assertFalse(isNotValid);
-	}
+	public void givenFirstName_WhenProper_ShouldReturnTrue1() {
 
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("Chethan");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("Chethan");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertTrue(isValid1);
+
+	}
+	public void givenFirstName_WhenShort_ShouldReturnFalse1() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("Ch");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("Ch");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	public void givenFirstName_WhenFirstLetterNotCapital_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("chethan");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("chethan");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenFirstName_Empty_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenFirstName_WhenContainsSpecialCharacters_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("Chethan@");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("Chethan@");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenFirstName_WhenContainsNumbers_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("Chethan123");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("Chethan123");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("Shetty");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("Shetty");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertTrue(isValid1);
+
+	}
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("Sh");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("Sh");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenLastName_WhenFirstLetterNotCapital_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("shetty");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("shetty");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenLastName_WhenEmpty_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenLastName_WhenContainsSpecialCharacters_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("Shetty@");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("Shetty@");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenLastName_WhenContainsNumbers_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateName("shetty123");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateName("shetty123");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenEmail_WhenValid_ShouldReturnTrue1() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateEMail("abc@gmail.com");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateEMail("abc@gmail.com");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertTrue(isValid1);
+
+	}
+	public void givenEmail_WhenInValid_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateEMail("abcgmail.com");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateEMail("abcgmail.com");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenEmail_WhenEmpty_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateEMail("");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateEMail("");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPhoneNumber_WhenValid_ShouldReturnTrue() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateMobileNumber("91 9191123498");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateMobileNumber("91 9191123498");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertTrue(isValid1);
+
+	}
+	@Test
+	public void givenPhoneNumber_WhenInValid_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateMobileNumber("123456789");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateMobileNumber("123456789");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPhoneNumber_WhenEmpty_ShouldReturnFalse() {
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid = userValidator.validateMobileNumber("");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validateMobileNumber("");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPassword_WhenValid_ShouldReturnTrue() 
+	{
+
+		UserValidation userValidator = new UserValidation();
+		boolean isValid=userValidator.validatePassword("Abc12@we3");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validatePassword("Abc12@we3");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertTrue(isValid1);
+	}
+	@Test
+	public void givenPassword_WhenLessThanEightCharacters_ShouldReturnFalse()
+	{
+		UserValidation userValidator = new UserValidation();
+		boolean isValid=userValidator.validatePassword("A3bc1@");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validatePassword("A3bc1@");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPassword_WhenNoUpperCaseLetters_ShouldReturnFalse()
+	{
+		UserValidation userValidator = new UserValidation();
+		boolean isValid=userValidator.validatePassword("azaz3bc1@");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validatePassword("azaz3bc1@");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPassword_WhenNoNumbers_ShouldReturnFalse()
+	{
+		UserValidation userValidator = new UserValidation();
+		boolean isValid=userValidator.validatePassword("Abcdef@ghij");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validatePassword("Abcdef@ghij");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPassword_WhenNoSpecialCharacters_ShouldReturnFalse()
+	{
+		UserValidation userValidator = new UserValidation();
+		boolean isValid=userValidator.validatePassword("Abc123456av");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validatePassword("Abc123456av");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPassword_WhenMoreThanOneSpecialCharacters_ShouldReturnFalse()
+	{
+		UserValidation userValidator = new UserValidation();
+		boolean isValid=userValidator.validatePassword("Abc12345@@@");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validatePassword("Abc12345@@@");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
+	@Test
+	public void givenPassword_WhenEmpty_ShouldReturnFalse()
+	{
+		UserValidation userValidator = new UserValidation();
+		boolean isValid=userValidator.validatePassword("");
+		boolean isValid1 = false;
+		try {
+			isValid1 = userValidator.validatePassword("");
+		} catch (UserValidatorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Assert.assertFalse(isValid1);
+
+	}
 }
